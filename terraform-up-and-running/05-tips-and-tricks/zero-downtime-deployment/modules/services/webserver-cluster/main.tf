b3c10965b86014e42879992170fead990112a457
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-12-11 01:03:32 -0300

    (terraform-book) zero downtime use a min elb capacity

--- terraform-up-and-running/05-tips-and-tricks/zero-downtime-deployment/modules/services/webserver-cluster/main.tf
+++ terraform-up-and-running/05-tips-and-tricks/zero-downtime-deployment/modules/services/webserver-cluster/main.tf
@@ -22,14 +22,16 @@ data "template_file" "user_data" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  name = "${var.cluster_name}-${aws_launch_configuration.example.name}"
+
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
   target_group_arns = [
     aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
-
   min_size = var.min_size
   max_size = var.max_size
+  min_elb_capacity = var.min_size
 
   tag {
     key = "Name"
