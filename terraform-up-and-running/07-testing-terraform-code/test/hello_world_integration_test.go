b10af00fc1c6e1e6ed2b2b72886ee604b09b4705
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-12-14 16:47:53 -0300

    (terraform-book) infrastructure integration test separted in stages

--- terraform-up-and-running/07-testing-terraform-code/test/hello_world_integration_test.go
+++ terraform-up-and-running/07-testing-terraform-code/test/hello_world_integration_test.go
@@ -2,9 +2,10 @@ package test
 
 import (
 	"fmt"
-	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	httpHelper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"strings"
 	"testing"
 	"time"
@@ -31,40 +32,6 @@ func TestHelloWorldAppStage(t *testing.T) {
 	validateHelloApp(t, helloOpts)
 }
 
-func createHelloOpts(
-	dbOpts *terraform.Options,
-	terraformDir string) *terraform.Options {
-
-	return &terraform.Options{
-		TerraformDir: terraformDir,
-
-		Vars: map[string]interface{}{
-			"db_remote_state_bucket": dbOpts.BackendConfig["bucket"],
-			"db_remote_state_key":    dbOpts.BackendConfig["key"],
-			"environment":            dbOpts.Vars["db_name"],
-		},
-	}
-}
-
-func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
-	albDnsName := terraform.OutputRequired(t, helloOpts, "alb_dns_name")
-	url := fmt.Sprintf("http://%s", albDnsName)
-
-	maxRetries := 10
-	timeBetweenRetries := 10 * time.Second
-
-	http_helper.HttpGetWithRetryWithCustomValidation(
-		t,
-		url, nil,
-		maxRetries,
-		timeBetweenRetries,
-		func(status int, body string) bool {
-			return status == 200 &&
-				strings.Contains(body, "Hello, World")
-		},
-	)
-}
-
 func createDbOpts(t *testing.T, terraformDir string) *terraform.Options {
 	uniqueId := random.UniqueId()
 
@@ -86,4 +53,86 @@ func createDbOpts(t *testing.T, terraformDir string) *terraform.Options {
 			"encrypt": true,
 		},
 	}
+}
+
+func createHelloOpts(dbOpts *terraform.Options, terraformDir string) *terraform.Options {
+
+	return &terraform.Options{
+		TerraformDir: terraformDir,
+
+		Vars: map[string]interface{}{
+			"db_remote_state_bucket": dbOpts.BackendConfig["bucket"],
+			"db_remote_state_key":    dbOpts.BackendConfig["key"],
+			"environment":            dbOpts.Vars["db_name"],
+		},
+	}
+}
+
+func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
+	albDnsName := terraform.OutputRequired(t, helloOpts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDnsName)
+
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	httpHelper.HttpGetWithRetryWithCustomValidation(
+		t,
+		url, nil,
+		maxRetries,
+		timeBetweenRetries,
+		func(status int, body string) bool {
+			return status == 200 &&
+				strings.Contains(body, "Hello, World")
+		},
+	)
+}
+
+func TestHelloWorldAppStageWithStages(t *testing.T) {
+	t.Parallel()
+	// Store the functions in a variable solely.
+	stage := test_structure.RunTestStage
+
+	// Deploy RDS
+	defer stage(t, "teardown_db", func() { teardownDb(t, dbDirStage) })
+	stage(t, "deploy_db", func() { deployDb(t, dbDirStage) })
+
+	// Deploy the hello-world-app
+	defer stage(t, "teardown_app", func() { teardownApp(t, appDirStage) })
+	stage(t, "deploy_app", func() { deployApp(t, dbDirStage, appDirStage) })
+
+	// Validate the hello-world-app works
+	stage(t, "validate_app", func() { validateApp(t, appDirStage) })
+}
+
+func teardownDb(t *testing.T, dbDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	defer terraform.Destroy(t, dbOpts)
+}
+
+func deployDb(t *testing.T, dbDir string) {
+	dbOpts := createDbOpts(t, dbDir)
+	// Save data to disk so that other test stages executed at a later
+	// time can read the data back in
+	test_structure.SaveTerraformOptions(t, dbDir, dbOpts)
+
+	terraform.InitAndApply(t, dbOpts)
+}
+
+func deployApp(t *testing.T, dbDir string, helloAppDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	helloOpts := createHelloOpts(dbOpts, helloAppDir)
+	// Save data to disk so that other test stages executed at a later
+	// time can read the data back in
+	test_structure.SaveTerraformOptions(t, helloAppDir, helloOpts)
+
+	terraform.InitAndApply(t, helloOpts)
+}
+func teardownApp(t *testing.T, helloAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+	defer terraform.Destroy(t, helloOpts)
+}
+
+func validateApp(t *testing.T, helloAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+	validateHelloApp(t, helloOpts)
 }
