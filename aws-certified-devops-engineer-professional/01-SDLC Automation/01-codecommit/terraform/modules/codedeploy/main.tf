9867cce6507c6085fe0ae8a38428fff671fc6704
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-12-24 13:34:26 -0300

    (aws-devops): added ec2 instance profile for codedeploy

--- aws-certified-devops-engineer-professional/01-SDLC Automation/01-codecommit/terraform/modules/codedeploy/main.tf
+++ aws-certified-devops-engineer-professional/01-SDLC Automation/01-codecommit/terraform/modules/codedeploy/main.tf
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# Create IAM Role for CodeDeploy to execute by assuming role and service-role
+# Create IAM Role for CodeDeploy group by assuming role and service-role
 # Provider Docs: https://www.terraform.io/docs/providers/aws/r/iam_role.html
 # Data Docs: https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
 # ---------------------------------------------------------------------------------------------------------------------
@@ -12,6 +12,29 @@ data "aws_iam_policy_document" "codedeploy" {
     actions = ["sts:AssumeRole"]
   }
 }
+resource "aws_iam_policy" "policy" {
+  name = "s3-artifacts-access"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bucket_deploy_revisions.arn}",
+        "${aws_s3_bucket.bucket_deploy_revisions.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
 resource "aws_iam_role" "this" {
   name = "CodeDeployRole"
   assume_role_policy = data.aws_iam_policy_document.codedeploy.json
@@ -20,6 +43,7 @@ resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
   role = aws_iam_role.this.name
 }
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Create CodeDeploy Application
 # Provider Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codedeploy_app
@@ -58,8 +82,13 @@ resource "aws_security_group" "instance" {
     from_port = 80
     protocol = "tcp"
     to_port = 80
-    cidr_blocks = [
-      "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "webserver_sg"
@@ -71,20 +100,40 @@ resource "aws_security_group" "instance" {
 # Provider Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 # Data Docs: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-instance.html
 # ---------------------------------------------------------------------------------------------------------------------
-
+data "aws_iam_policy_document" "ec2" {
+  statement {
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "ec2_role" {
+  name = "EC2RoleCodeDeploy"
+  assume_role_policy = data.aws_iam_policy_document.ec2.json
+}
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployS3Role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+  role = aws_iam_role.ec2_role.name
+}
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_codedeploy_profile"
+  role = aws_iam_role.ec2_role.name
+}
 resource "aws_instance" "web_server" {
   count = var.tier == "dev" ? 1:3
   // amazon linux 2  | us-east-1
   ami = "ami-04d29b6f966df1537"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "web_server"
     Environment = var.tier == "dev" ? "Development": "Production"
   }
   user_data = file("${path.module}/user-data.sh")
 }
-
 # ---------------------------------------------------------------------------------------------------------------------
 # Create an S3 Bucket with versioning and encryption
 # Provider Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
