87aa73d6e6d346d82974c3370f685f20a56831f7
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-01-17 22:31:21 -0300

    chore: deleted terragrunt cache

--- terraform-up-and-running/08-terraform-team/live/stage/data-stores/mysql/.terragrunt-cache/H7lEYCLSDRlEhjeGxCTveM4ru2A/XgfNQi--qqnOs0O_7N9SMbqzlic/networking/alb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "alb_name" {
-  description = "The name to use for this ALB"
-  type = string
-}
-
-variable "subnets_ids" {
-  description = "The subnet IDs to deploy to"
-  type = list(string)
-}
