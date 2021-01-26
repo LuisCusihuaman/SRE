64f53fb089c221b6c05c2e38230b0d0576814c2b
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-01-26 00:25:20 -0300

    fix(effective-devops): update test dependencies package.json

--- effective-devops-with-aws/05-ci-cd/helloworld/test/helloworld_test.js
+++ effective-devops-with-aws/05-ci-cd/helloworld/test/helloworld_test.js
@@ -1,17 +1,16 @@
-var Browser = require('zombie')
-var assert = require('assert')
-var app = require('../helloworld')
+const Browser = require("zombie");
+const assert = require("assert");
+const app = require("../helloworld");
 
-
-describe('main page', function() { 
-  before(function() {
-    this.browser = new Browser({ site: 'http://localhost:3000' })
-  })
-  before(function(done) {
-    this.browser.visit('/', done)
-  })
-  it('should say hello world', function() { 
-    assert.ok(this.browser.success)
-    assert.equal(this.browser.text(), "Hello World")
-  })
-})
+describe("main page", function () {
+  before(function () {
+    this.browser = new Browser({ site: "http://localhost:3000" });
+  });
+  before(function (done) {
+    this.browser.visit("/", done);
+  });
+  it("should say hello world", function () {
+    assert.ok(this.browser.success);
+    assert.strictEqual(this.browser.text(), "Hello World");
+  });
+});
