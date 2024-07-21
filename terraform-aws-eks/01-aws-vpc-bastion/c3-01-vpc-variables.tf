b097d35bde0b12dd2cc4e42d75c3296cd2cc8028
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-07-21 12:25:19 -0300

    feat(eks/vpc-demo): ec2-sg module

--- terraform-aws-eks/01-aws-vpc-bastion/c3-01-vpc-variables.tf
+++ terraform-aws-eks/01-aws-vpc-bastion/c3-01-vpc-variables.tf
@@ -11,7 +11,7 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type = string 
-  default = "10.0.0.0/16"
+  default = "20.0.0.0/16"
 }
 
 # VPC Availability Zones
@@ -27,21 +27,21 @@ variable "vpc_availability_zones" {
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  default = ["20.0.101.0/24", "20.0.102.0/24"]
 }
 
 # VPC Private Subnets
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["20.0.1.0/24", "20.0.2.0/24"]
 }
 
 # VPC Database Subnets
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+  default = ["20.0.151.0/24", "20.0.152.0/24"]
 }
 
 # VPC Create Database Subnet Group (True / False)
