623be406c025714e2e07329a9dde54c60431a4e3
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-07-23 08:50:24 -0300

    feat(eks): horizontal pod autoscaler

--- terraform-aws-eks/15-aws-eks-horizontal-pod-autoscaler/c1-versions.tf
+++ terraform-aws-eks/15-aws-eks-horizontal-pod-autoscaler/c1-versions.tf
@@ -8,32 +8,19 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.14.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.4.3"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.31.0"
-    }
   }
   # Remote State Storage
   backend "s3" {
     bucket = "cusihuaman-on-aws-eks" # CHANGE THIS
-    key    = "dev/aws-eks-autoscaler/terraform.tfstate"
+    key    = "dev/aws-eks-metrics-server/terraform.tfstate"
     region = "us-east-1"
 
     # For State Locking
-    dynamodb_table = "dev-aws-eks-autoscaler"
+    dynamodb_table = "aws-eks-metrics-server"
   }
 }
 
 # Terraform AWS Provider Block
 provider "aws" {
   region = var.aws_region
-}
-
-# Terraform HTTP Provider Block
-provider "http" {
-  # Configuration options
 }
